a170a2d62d92574c790f57d573da8c940541a8ea
unknown <mattloe67@gmail.com>
2020-07-07 14:56:10 +0800

    Created string reverse, iterative + recursive

---
+++ StringChallenges/StringReverse.java
@@ -0,0 +1,38 @@
+public class StringReverse
+{
+    public static void main(String args[])
+    {
+        String input = "Hello World";
+        String output = "";
+        char[] arr = input.toCharArray();
+        
+        System.out.println("Reverse a string.");
+        System.out.println("Iteratively");
+        System.out.println("Input: "+input);
+
+        for (int ii = input.length() - 1; ii >= 0; ii--)
+        {
+            output = output + arr[ii];
+        }
+        //END FOR
+
+        System.out.println("Output: " + output);
+
+        System.out.println("Recursively");
+
+        System.out.println("Output: "+ reverse(input));
+    }
+
+    private static String reverse(String str)
+    {
+        System.out.println(str);
+
+        if (str.length() < 2)   //Base Case
+        {
+            return str;
+        }
+
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+    
+}
